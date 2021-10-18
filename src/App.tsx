a4b03b8a3583470fbd6dd4785be732529d2ef9ef
dariendaud <darien.daud@yahoo.com>
2021-10-18 20:41:02 +0700

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/header";
 import Home from './pages/home';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home}></Route>
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="container-fluid px-0">
+        <Header />
+
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return(
-      <div>Header</div>
+    return (
+      <nav className="navbar navbar-expand-md navbar-light navbar-border-bottom background-color-theme ps-4">
+        <a className="navbar-brand text-center" href="/">
+          <img src={process.env.PUBLIC_URL + '/image/pokeball.png'} width="50px" />
+        </a>
+        <span className="logo-title ms-4">Pokedex</span>
+      </nav>
     );
   }
 }
